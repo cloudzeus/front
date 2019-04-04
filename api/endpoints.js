b6f1ce3d2beec58f16637ae47c491d7d49a71c66
cloudzeus <gkozyris@cloudzeus.eu>
2019-04-04 17:44:20 +0200

    fixing endpoint domain

--- api/endpoints.js
+++ api/endpoints.js
@@ -2,5 +2,5 @@ const endpoints = {
     getPricelist : 'http://ersPappas.intellisoft.gr:40004/api/Availability/GetPriceLists',
     getCategories : 'http://ersPappas.intellisoft.gr:40004/api/Availability/CheckVehicleAvailability',
     newBooking : 'http://erspappas.intellisoft.gr:40004/api/Bookings/AddNewBooking',
-    newReservation:"http://localhost:7000/api/reservations/create", //@TODO change in production
+    newReservation:"https://backend.breezerentals.gr/api/reservations/create", //@TODO change in production
 }
