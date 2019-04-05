0e933145eac392f23eea522865052c060feafa11
cloudzeus <gkozyris@cloudzeus.eu>
2019-04-05 11:25:55 +0200

    changing the backoffice  api domain

--- api/endpoints.js
+++ api/endpoints.js
@@ -1,6 +1,6 @@
 const endpoints = {
-    getPricelist : 'http://ersPappas.intellisoft.gr:40004/api/Availability/GetPriceLists',
-    getCategories : 'http://ersPappas.intellisoft.gr:40004/api/Availability/CheckVehicleAvailability',
-    newBooking : 'http://erspappas.intellisoft.gr:40004/api/Bookings/AddNewBooking',
+    getPricelist : 'https://ersPappas.intellisoft.gr/api/Availability/GetPriceLists',
+    getCategories : 'https://ersPappas.intellisoft.gr/api/Availability/CheckVehicleAvailability',
+    newBooking : 'https://erspappas.intellisoft.gr/api/Bookings/AddNewBooking',
     newReservation:"https://backend.breezerentals.gr/api/reservations/create", //@TODO change in production
 }
